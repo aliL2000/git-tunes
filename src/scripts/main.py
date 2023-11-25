54b8678629e057336570f9cfd1dd946878eaae19
aliL2000 <aliladha2000@gmail.com>
2023-11-25 16:15:47 -0500

    Getting eel integrated to the system.

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -1,40 +1,33 @@
-import PySimpleGUI as sg
 from authorization.spotify_auth import get_token
 from spotify_api.playlist_data import get_playlist_by_id, get_songs_from_playlist, get_differences
+import eel
+import os
+
+eel.init(os.path.dirname(__file__) + str("/web"))
+eel.start("index.html", port = 8080)
+
+
+@eel.expose                         # Expose this function to Javascript
+def say_hello_py(x):
+    print('Hello from %s' % x)
+
+say_hello_py('Python World!')
+eel.say_hello_js('Python World!')   # Call a Javascript function
+
+eel.start('hello.html')             # Start (this blocks and enters loop)
 
 
 
 
+# token = get_token()
+# json_first_playlist = get_playlist_by_id(token,"46Vvp6M1HPCbNg1W9kcCRZ")
+# json_second_playlist = get_playlist_by_id(token,"0CtD0CLuF8cXpLAN1H07jO")
+# first_playlist_songs = get_songs_from_playlist(json_first_playlist)
+# second_playlist_songs = get_songs_from_playlist(json_second_playlist)
 
-token = get_token()
-json_first_playlist = get_playlist_by_id(token,"46Vvp6M1HPCbNg1W9kcCRZ")
-json_second_playlist = get_playlist_by_id(token,"0CtD0CLuF8cXpLAN1H07jO")
-first_playlist_songs = get_songs_from_playlist(json_first_playlist)
-second_playlist_songs = get_songs_from_playlist(json_second_playlist)
-
-different_songs = get_differences(first_playlist_songs,second_playlist_songs)
-print(different_songs.keys())
-
-
-
-# layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
+# different_songs = get_differences(first_playlist_songs,second_playlist_songs)
+# print(different_songs.keys())
 
 
 
-# # Create the window
-# window = sg.Window("Git-Tunes", layout)
-
-# # Create an event loop
-# while True:
-#     event, values = window.read()
-#     # End program if user closes window or
-#     # presses the OK button
-#     if event == "OK" or event == sg.WIN_CLOSED:
-#         break
-
-# window.close()
-
-# f = open("demofile3.txt", "w")
-# f.write(json.dumps(json_results,indent=4))
-# f.close()
-
+#https://realpython.com/pysimplegui-python/#packaging-your-pysimplegui-application-for-windows
